c9ebe16794e03e6605115798ba6aa26e5c002e98
ric <ric@netrc.com>
2025-01-18 14:34:24 -0500

    better reducer for name index done

--- db.js
+++ db.js
@@ -12,13 +12,12 @@ const keysToField = (obj,field) => (a,c) => { a[obj[c][field]] = obj[c]; return 
 const keysObjToField = (field) => (obj) => Object.keys(obj).reduce( keysToField(obj,field), {} )
 const keysObjToName = keysObjToField('name')
 
-//const keysObjToName = obj => Object.keys(obj).reduce( (a,c) => { a[obj[c]['name']] = obj[c]; return a}, {} )
 
 const setup = async (server) => {
   const my_server = await findDBservice.checkServers()
 
-  const db = { }
   const dburl = dburlServer(my_server.url)
+  const db = { }
   db.check = await fetch(dburl('check')).then( r => r.json() );
   db.churches = await fetch(dburl('churches')).then( r => r.json() );
   db.brasses = await fetch(dburl('brasses')).then( r => r.json() );
@@ -27,10 +26,11 @@ const setup = async (server) => {
   db.notes = await fetch(dburl('notes')).then( r => r.json() );
 
   // do some indexing, filtering
-  //db.churchesByName = Object.keys(db.churches).reduce( (a,c) => { a[db.churches[c].name] = db.churches[c]; return a},  {})
   db.churchesByName = keysObjToName(db.churches)
-  db.brassesByName = Object.keys(db.brasses).reduce( (a,c) => { a[c.name] = c; return a},  {})
-  db.rubbingssByName = Object.keys(db.rubbings).reduce( (a,c) => { a[c.name] = c; return a},  {})
+  db.brassesByName = keysObjToName(db.brasses)
+  db.rubbingssByName = keysObjToName(db.rubbings)
+
+  // misc info
   db.my_server = my_server
   console.log(`check: ${JSON.stringify(db.check)}`)
   return db
